c31b8699a6c381ce46d10bbfb87c6b0d31382a90
Aseem S <aseems2019@gmail.com>
2026-09-20 08:08:51 +0530

    Fix Linux deployment dependency

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: [
-    "@napi-rs/canvas",
-    "@napi-rs/canvas-win32-x64-msvc",
-  ],
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  serverExternalPackages: ["@napi-rs/canvas"],
 };
 
 export default nextConfig;
